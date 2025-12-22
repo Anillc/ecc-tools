f6b1bd59ef3f81935cf458af2f658b682888be81
zxy <zhaoxueyan1@outlook.com>
2025-12-22 13:52:02 +0800

    restrict congestion map max value at 2.5

--- src/interface/python/py_imp/idb_to_imp_db/PyPlaceDBRoutbility.cpp
+++ src/interface/python/py_imp/idb_to_imp_db/PyPlaceDBRoutbility.cpp
@@ -315,11 +315,15 @@ std::vector<std::vector<float>> PyPlaceDB::getCongestionMap(string method, strin
           sum_demand_map[i][j] += demand_val;
         } else if (method == "max") {
           if (supply_val == 0) {
-            float tmp_val = std::min(1 + 1. * demand_val / 2, 4.0);
+            float tmp_val = std::min(1 + 1. * demand_val / 2, 2.5);
             result_map[i][j] = std::max(result_map[i][j], 1.f * tmp_val);
-            continue;
+          } else {
+            result_map[i][j] = std::max(result_map[i][j], 1.f * demand_val / supply_val);
           }
-          result_map[i][j] = std::max(result_map[i][j], 1.f * demand_val / supply_val);
+          if (result_map[i][j] > 2.5) {
+            result_map[i][j] = 2.5;
+            // printf("Warning: too large for congestion map\n");
+          }
         } else {
           std::cerr << "Error: unsupported method " << method << ", use sum instead." << std::endl;
           sum_supply_map[i][j] += supply_val;
